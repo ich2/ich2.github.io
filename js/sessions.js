70795ae49d029155831e7aeba90df1d40f1821a7
ich2 <imenechentli1@gmail.com>
2024-02-20 21:00:56 +0100

    Add function in sessions.js

--- js/sessions.js
+++ js/sessions.js
@@ -1,3 +1,11 @@
 // exercice 1
 document.getElementById("prix_ht").innerHTML = Math.floor(Math.random() * 13)  + 100;
 document.getElementById("marge").innerHTML = (Math.floor(Math.random() * 13)  + 20);
+
+// passwords
+function validate_password() {
+  var x= document.getElementById("password");
+  var y= document.getElementById("password_again");
+  if(x.value==y.value) return;
+  else alert("Les mots de passe saisis sont différents.");
+}
