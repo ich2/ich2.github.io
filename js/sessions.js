0c7ad325ff36ad8e16db86964ecad05060f7d1e8
ich2 <imenechentli1@gmail.com>
2024-02-20 21:05:37 +0100

    Update sessions.js

--- js/sessions.js
+++ js/sessions.js
@@ -3,9 +3,11 @@ document.getElementById("prix_ht").innerHTML = Math.floor(Math.random() * 13)  +
 document.getElementById("marge").innerHTML = (Math.floor(Math.random() * 13)  + 20);
 
 // passwords
+<script>
 function validate_password() {
   var x= document.getElementById("password");
   var y= document.getElementById("password_again");
   if(x.value==y.value) return x;
   else alert("Les mots de passe saisis sont différents.");
 }
+</script>
