44aec6ba00ed3941d2560435beda71e92bd82fd5
ich2 <imenechentli1@gmail.com>
2024-02-20 01:20:22 +0100

    Create sessions.js

---
+++ js/sessions.js
@@ -0,0 +1,3 @@
+// exercice 1
+document.getElementById("prix_ht").innerHTML = Math.floor(Math.random() * 13)  + 100;
+document.getElementById("marge").innerHTML = (Math.floor(Math.random() * 13)  + 20);
